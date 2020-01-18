5a0688f3104f42b8ec8d689d332e279403478de7
DentonWarnock <dentonwarnock@gmail.com>
2020-01-17 16:32:42 -0800

    .get endpoint "/" added and returns all posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -3,4 +3,19 @@ const db = require("../data/db");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  db.find(req.body)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({
+          error: "There was an error while saving the comment to the database"
+        });
+    });
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const postsRouter = require("./posts/posts-router");
 const server = express();
 
 server.use(express.json());
-server.use("api/posts", postsRouter);
+server.use("/api/posts", postsRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Denton's Posts API</h2>`);
