e50b5e7b104c0764f77d96cdf1e4fe7510c28ca3
DentonWarnock <dentonwarnock@gmail.com>
2020-01-17 20:22:38 -0800

    added front end to view list of posts

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const postsRouter = require("./posts/posts-router");
+const cors = require("cors");
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 server.use("/api/posts", postsRouter);
 
 server.get("/", (req, res) => {
