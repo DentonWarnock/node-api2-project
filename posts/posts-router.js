3eada14750edc260ad7c044d3a3627a767951c09
DentonWarnock <dentonwarnock@gmail.com>
2020-01-17 19:04:39 -0800

    .post endpoint "/:id/comment" added and returns newly created comment

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -3,6 +3,7 @@ const db = require("../data/db");
 
 const router = express.Router();
 
+//Creates a post using the information sent inside the request body.
 router.post("/", (req, res) => {
   const { title, contents } = req.body;
   if (!title || !contents) {
@@ -11,9 +12,9 @@ router.post("/", (req, res) => {
     });
   } else {
     db.insert(req.body)
-      .then(id => {
+      .then(({ id }) => {
         if (id) {
-          db.findById(id.id).then(post => {
+          db.findById(id).then(post => {
             // console.log(post);
             res.status(201).json(post);
           });
@@ -27,6 +28,44 @@ router.post("/", (req, res) => {
   }
 });
 
+//Creates a comment for the post with the specified id using information sent inside of the request body.
+router.post("/:id/comments", (req, res) => {
+  const { text } = req.body;
+  const post_id = req.params.id;
+
+  if (!text) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide text for the comment." });
+  } else {
+    db.findById(post_id).then(post => {
+      if (!post) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      } else {
+        let newComment = {
+          text: text,
+          post_id: post_id
+        };
+        db.insertComment(newComment)
+          .then(({ id }) => {
+            db.findCommentById(id).then(comment => {
+              res.status(201).json(comment);
+            });
+          })
+          .catch(err => {
+            res.status(500).json({
+              error:
+                "There was an error while saving the comment to the database"
+            });
+          });
+      }
+    });
+  }
+});
+
+//Returns an array of all the post objects contained in the database.
 router.get("/", (req, res) => {
   db.find(req.body)
     .then(posts => {
@@ -39,5 +78,15 @@ router.get("/", (req, res) => {
       });
     });
 });
+
+//Returns the post object with the specified id.
+
+//Returns an array of all the comment objects associated with the post with the specified id.
+
+//Removes the post with the specified id and returns the deleted post object.
+//You may need to make additional calls to the database in order to satisfy this requirement.
+
+//Updates the post with the specified id using data from the request body.
+//Returns the modified document, NOT the original.
 
 module.exports = router;
