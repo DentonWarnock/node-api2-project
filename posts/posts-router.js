31d1f010d30919e97e8eb0c2d9c5b1062fc48c9e
DentonWarnock <dentonwarnock@gmail.com>
2020-01-17 17:36:38 -0800

    .post endpoint "/" added and returns newly created post

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -3,6 +3,30 @@ const db = require("../data/db");
 
 const router = express.Router();
 
+router.post("/", (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  } else {
+    db.insert(req.body)
+      .then(id => {
+        if (id) {
+          db.findById(id.id).then(post => {
+            // console.log(post);
+            res.status(201).json(post);
+          });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({
+          message: "There was an error while saving the post to the database"
+        });
+      });
+  }
+});
+
 router.get("/", (req, res) => {
   db.find(req.body)
     .then(posts => {
@@ -10,11 +34,9 @@ router.get("/", (req, res) => {
     })
     .catch(error => {
       console.log(error);
-      res
-        .status(500)
-        .json({
-          error: "There was an error while saving the comment to the database"
-        });
+      res.status(500).json({
+        error: "There was an error while saving the comment to the database"
+      });
     });
 });
 
